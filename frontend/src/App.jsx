cf443156ca78ff26fe0bf1dc5267830011f35462
AAKASH KUMAR <162182656+akprajapat5501@users.noreply.github.com>
2026-01-22 00:39:51 +0530

    Update API endpoint for fetching user data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
   useEffect(()=>{
     const fetchUser = async()=>{
       try {
-        const response = await axios.get("http://localhost:4000/api/v1/user/patient/me", {withCredentials: true});
+        const response = await axios.get("https://hospital-management-system-backend-1lms.onrender.com/api/v1/user/patient/me", {withCredentials: true});
         if(response.data.success){
           setIsAuthenticated(true);
           setUser(response.data.user);
